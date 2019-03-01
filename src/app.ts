4e29447d00937b011145e3c68a8d92e0d5e83fed
Johny <johny.premanantham@adssets.com>
2019-03-01 16:36:32 +0100

    setup project

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello TypeScript!');
+console.log('adas TypeScript!');
+
+const test = [
+    {key: 'value', key1: 'value1'}
+]
+
+const mappedTests = test.map( test => test.key.toUpperCase());
+
+console.log(mappedTests);
+
+function multiply(a: number, b: number = 10) {
+    return a * b;
+}
+
+console.log(multiply(10));
